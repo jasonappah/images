7bca0cc847e044c0026496bafc890cce1eb44dab
Jason Antwi-Appah <hey@jasonaa.me>
2021-04-23 19:32:55 -0500

    feat: make proxy

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
-import bodyParser from "body-parser";
 import express from "express";
+const fetch = require("node-fetch");
 
 const app = express();
 const port = process.env.PORT || 3333;
+const src = process.env.SRC_URL;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+const ts = () => new Date().toLocaleString();
+const logger = (msg: string, func: any) => func(ts(), msg);
+const log = (msg: string) => logger(msg, console.log);
+const err = (msg: string) => logger(msg, console.error);
 
-app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
-});
+try {
+  app.get("/", async (req: express.Request, res: express.Response) => {
+    res.json({
+      yo: "hi",
+      socials: {
+        web: "https://jasonaa.me",
+        twitter: "https://jasonaa.me/t",
+        github: "https://jasonaa.me/g",
+      },
+      src: "https://jasonaa.me/g/images",
+    });
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  app.get("*", async (req: express.Request, res: express.Response) => {
+    const url = src + req.originalUrl;
+    log(`Proxying ${url}...`);
+    const dl = await fetch(url);
+    res.end(Buffer.from(await dl.arrayBuffer()));
+  });
+
+  app.listen(port, () => {
+    log(`⚡ Proxying all requests from http://localhost:${port} to ${src}`);
+  });
+} catch (e) {
+  err(e);
+}
